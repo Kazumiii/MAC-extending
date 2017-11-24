7b77887315a6a91f49f3a5ef7b977c12910a9cfd
Szymon Ratajczak <daniel.kruk1@interia.pl>
2017-11-24 18:55:29 +0100

    Add files via upload

---
+++ fully padded message.js
@@ -0,0 +1,27 @@
+<script src="sjcl.js"></script>
+<script>
+
+var secretBits=64;
+
+var msg="Jone Austen wrote Sense and Sensibility";
+
+var msgBits=msg.length*8+secretBits;
+
+var msgBitsHexString=msgBits.toString(16);
+
+var paddingHexString="8";
+
+var zeros=512-8msgBits-(16-msgBitsHexString.length);
+
+for(var i=0;i<zeros/8;++i)
+{
+	
+	paddingHexString+="00";
+}
+paddingHexString+=msgBitsHexString;
+
+var padding=sjcl.codec.hex.toBits(paddingHexString);
+
+
+
+</script>
